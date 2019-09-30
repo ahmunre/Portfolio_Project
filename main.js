7e271f5c03b3e258cbbe29d47a1ef7fb010d075b
Elton Griffith <ahmunre@yahoo.com>
2019-09-30 17:56:56 -0400

    made final changes to project files

---
+++ main.js
@@ -0,0 +1,40 @@
+        function selectBio()
+        {
+            selectTab("Bio");
+        }
+        function selectContacts()
+        {
+            selectTab("Contacts");
+        }
+        function selectProjects()
+        {
+            selectTab("Projects");
+        }
+        function selectTab(name)
+        {
+            var i;
+            // Get the navbar and the tabcontainer
+            var navbar = document.getElementById("nav_bar");
+            var tabArea = document.getElementById("tab_container");
+            // Remove the 'Selected' class from all children of the navbar and the tabs
+            // read: clear all tabs and buttons
+            var buttons = navbar.children;
+            for(i = 0; i < buttons.length; i++)
+            {
+                var button = buttons[i];
+                button.classList.remove("Selected");
+            }
+            var tabs = tabArea.children;
+            for(i = 0; i < tabs.length; i++)
+            {
+                var tab = tabs[i];
+                tab.classList.remove("Selected");
+            }
+            // Get the tab and button to select
+            var selectedButton = document.getElementsByClassName(name + "TabButton")[0];
+            var selectedTabBody = document.getElementsByClassName(name + "TabBody")[0];
+            // Add the 'Selected' class to the tab and the button
+            selectedButton.classList.add("Selected");
+            selectedTabBody.classList.add("Selected");
+            // Done.
+        }
